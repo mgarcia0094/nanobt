042bfb412680723e6a75adf472abfcbe6b1ab7de
Miguel Ángel García Gandía <miguel@MBP-de-Miguel.home>
2020-10-11 14:11:24 +0200

    Update gitignore

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nanobt", # Replace with your own username
+    name="nanobt",
     version="0.0.5",
     author="Miguel Ángel García Gandía",
     author_email="-",
